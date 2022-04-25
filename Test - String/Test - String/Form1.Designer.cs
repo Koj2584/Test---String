b38b6b3cd7e0ac8d28d56db7fc0f8d2725ec2e96
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2022-04-25 09:34:37 +0200

    Nahrání

---
+++ Test - String/Test - String/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Test___String
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 24);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(256, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox1.BackColor = System.Drawing.Color.Gray;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.ForeColor = System.Drawing.Color.Gold;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(356, 249);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(407, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 3;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(411, 87);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(476, 227);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(411, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(899, 320);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Test - String/Test - String/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test___String
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string vstup = textBox1.Text;
+            string abc = "abcdefghijklmnopqrstuvwxyz";
+            vstup = vstup.ToLower();
+
+            vstup = vstup.Trim();
+            while (vstup.Contains("  "))
+                vstup = vstup.Replace("  ", " ");
+
+            label1.Text = vstup;
+
+            string[] slova = vstup.Split(' ');
+
+            int max = 0;
+            char maxC = ' ';
+            string nevyskyt = "";
+            foreach(string slovo in slova)
+            {
+                foreach(char pismeno in slovo)
+                {
+                    int pomocna = pocet(pismeno, vstup);
+                    if (max < pomocna && abc.Contains(pismeno))
+                    {
+                        max = pomocna;
+                        maxC = pismeno;
+                    }
+                }
+            }
+
+            flowLayoutPanel1.Controls.Clear();
+            foreach (char c in abc)
+            {
+                int pomocna = pocet(c, vstup);
+                if (pomocna == 0)
+                {
+                    nevyskyt += c + " ";
+                    Button b = new Button();
+                    b.Width = 50;
+                    b.Height = 50;
+                    b.BackColor = Color.Gray;
+                    b.FlatStyle = FlatStyle.Flat;
+                    b.FlatAppearance.BorderColor = Color.DarkGray;
+                    b.FlatAppearance.BorderSize = 1;
+                    b.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(238)));
+                    b.ForeColor = Color.Gold;
+                    b.Text = c.ToString();
+                    b.Click += abcClick;
+                    flowLayoutPanel1.Controls.Add(b);
+                }
+                else
+                {
+                    Button b = new Button();
+                    b.Width = 50;
+                    b.Height = 50;
+                    b.BackColor = Color.Gray;
+                    b.FlatStyle = FlatStyle.Flat;
+                    b.FlatAppearance.BorderSize = 0;
+                    b.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(238)));
+                    b.ForeColor = Color.Gold;
+                    b.Text = c.ToString();
+                    b.Click += abcClick;
+                    flowLayoutPanel1.Controls.Add(b);
+                }
+            }
+
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Nejčastěji vyskytující se písmeno je ---> " + maxC);
+            listBox1.Items.Add("Na výběr máte ---> " + nevyskyt);
+        }
+
+        void abcClick(object sender, EventArgs e)
+        {
+            string spravne = "jak nam zabili Ferdinanda." +
+                "Prazdny retezec ma hodnotu ““ nebo string.Empty ." +
+                "Instance String je nemenna." +
+                "Halda je prakticky neomezena pamet.";
+            StringBuilder sb =new StringBuilder(label1.Text);
+            if (label1.Text.IndexOf('_') != -1)
+            {
+                sb[label1.Text.IndexOf('_')] = Convert.ToChar((sender as Button).Text);
+                label1.Text = sb.ToString();
+
+                string abc = "abcdefghijklmnopqrstuvwxyz";
+                flowLayoutPanel1.Controls.Clear();
+                foreach (char c in abc)
+                {
+                    int pomocna = pocet(c, label1.Text);
+                    if (pomocna == 0)
+                    {
+                        Button b = new Button();
+                        b.Width = 50;
+                        b.Height = 50;
+                        b.BackColor = Color.Gray;
+                        b.FlatStyle = FlatStyle.Flat;
+                        b.FlatAppearance.BorderColor = Color.DarkGray;
+                        b.FlatAppearance.BorderSize = 1;
+                        b.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(238)));
+                        b.ForeColor = Color.Gold;
+                        b.Text = c.ToString();
+                        b.Click += abcClick;
+                        flowLayoutPanel1.Controls.Add(b);
+                    }
+                    else
+                    {
+                        Button b = new Button();
+                        b.Width = 50;
+                        b.Height = 50;
+                        b.BackColor = Color.Gray;
+                        b.FlatStyle = FlatStyle.Flat;
+                        b.FlatAppearance.BorderSize = 0;
+                        b.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(238)));
+                        b.ForeColor = Color.Gold;
+                        b.Text = c.ToString();
+                        b.Click += abcClick;
+                        flowLayoutPanel1.Controls.Add(b);
+                    }
+                }
+                if (label1.Text.IndexOf('_') == -1)
+                    label1.Text = "Vyhrál jsi!!!";
+                label2.Text = "";
+            }
+            else
+                label1.Text = "Vyhrál jsi!!!";
+        }
+
+        int pocet(char hledane, string vCem)
+        {
+            if (vCem.Contains(hledane))
+            {
+                string[] pole = vCem.Split(' ');
+                int pocetPrvku = 0;
+
+                foreach (string slovo in pole)
+                {
+                    foreach (char p in slovo)
+                    {
+                        if (p == hledane)
+                            pocetPrvku++;
+                    }
+                }
+
+                return pocetPrvku;
+            }
+            else
+                return 0;
+        }
+    }
+}
